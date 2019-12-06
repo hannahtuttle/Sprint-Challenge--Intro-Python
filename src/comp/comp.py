36e008749d868e36ecdde100012b06f41e9334d3
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-12-05 18:32:12 -0800

    completed comp.py, passed all tests

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -25,29 +25,42 @@ humans = [
 # whose name starts with 'D':
 print("Starts with D:")
 a = []
+for n in humans:
+    if n.name[0] == 'D':
+        a.append(n.name)
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
 b = []
+for n in humans:
+    if n.name[-1] == 'e':
+        b.append(n.name)
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
 c = []
+for n in humans:
+    if n.name[0] == "C" or n.name[0] == "D" or n.name[0] == "E" or n.name[0] == "F" or n.name[0] == "G":
+        c.append(n.name)
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
 d = []
+for n in humans:
+    d.append(n.age +10)
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
 e = []
+for n in humans:
+    e.append(f'{n.name}-{n.age}')
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
@@ -55,6 +68,9 @@ print(e)
 # inclusive.
 print("Names and ages between 27 and 32:")
 f = []
+for n in humans:
+    if n.age >=27 and n.age <= 32:
+        f.append((n.name, n.age))
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
@@ -62,10 +78,14 @@ print(f)
 # The "humans" list should be unmodified.
 print("All names uppercase:")
 g = []
+for n in humans:
+    g.append(Human(n.name.upper(), n.age + 5))
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
 h = []
+for n in humans:
+    h.append(math.sqrt(n.age))
 print(h)
